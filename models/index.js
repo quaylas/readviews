c365e9d1d69083abc50860db82306503f9d81bbd
Malachi <malachialusa@gmail.com>
2021-02-26 15:59:50 -0700

    I fully tested our user api through insominia. The only weakness is the log in, but we need to build more of this before that can be fixed

--- models/index.js
+++ models/index.js
@@ -7,30 +7,33 @@ const Vote = require('./Vote');
 //data relations 
 
 User.hasMany(Review, {
-    foreignKey: 'User_id'
+    foreignKey: 'user_id'
 });
 
 Review.belongsTo(User, {
-    foreignKey: 'User_id',
+    foreignKey: 'user_id',
     onDelete: 'SET NULL'
 });
+
 
 User.belongsToMany(Review, {
     through: Vote,
     as: 'voted_reviews',
-    foreignKey: 'User_id',
+    foreignKey: 'user_id',
     onDelete: 'SET NULL'
 });
 
 Review.belongsToMany(User, {
     through: Vote,
     as: 'voted_reviews',
-    foreignKey: 'Review_id',
+    foreignKey: 'review_id',
     onDelete: 'SET NULL'
 });
 
+/*
+
 Vote.belongsTo(User, {
-    foreignKey: 'User_id',
+    foreignKey: 'user_id',
     onDelete: 'SET NULL'
 });
 
@@ -45,36 +48,42 @@ User.hasMany(Vote, {
 
 Review.hasMany(Vote, {
     foreignKey: 'Review_id'
-});
+}); 
+*/
 
 Comment.belongsTo(User, {
-    foreignKey: 'Review_id',
+    foreignKey: 'user_id', //changed from Review_id
     onDelete: 'SET NULL'
 });
 
 Comment.belongsTo(Review, {
-    foreignKey: 'Comment_id',
+    foreignKey: 'review_id', //changed from comment_id
     onDelete: 'SET NULL'
 });
 
 User.hasMany(Comment, {
-    foreignKey: 'User_id',
+    foreignKey: 'user_id',
     onDelete: 'SET NULL'
 });
 
+
+/*
+
 Review.hasMany(Comment, {
-    foreignKey: 'Review_id'
+    foreignKey: 'review_id'
 });
+
+*/
 
  //Book relations 
 
-Book.hasMany(Review, {
+/*Book.hasMany(Review, {
     foreignKey: 'book_id'
 });
 
 Review.belongsTo(Book, {
     foreignKey: 'book_id'
-});
+}); */
 
 
 module.exports = { User, Review, Vote,  Book, Comment };
