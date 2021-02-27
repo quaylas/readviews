7796e0f57a4b7d24e2c76c626cc68656ae874ba7
quaylas <quayla@cxadvisors.com>
2021-02-27 11:36:48 -0700

    updated with routes to search book by title and author

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 const bookRoutes =  require('./book-routes.js');
-const reviewRoutes =  require('./review-routes');
+const reviewRoutes =  require('./review-routes.js');
 
 router.use('/users', userRoutes);
 router.use('/books', bookRoutes);
